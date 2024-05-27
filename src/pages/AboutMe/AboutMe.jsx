b57f1a13b8c48074b8e971180ce52f5a5bb5c294
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-05-26 23:32:00 -0600

    My picture's issue fixed

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 import Layout from '../../components/Layout/Layout.jsx'
 import Introduction from '../../sections/Introduction/Introduction.jsx'
