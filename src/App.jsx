11ef26300f7f9cc7ddd624b3f7a9c6d5fcfcab75
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2025-02-02 21:07:42 -0600

    Lets give it another try addressing building issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from '@/pages/home'
-import Layout from '@/pages/Layout'
-import Project from '@/pages/project'
+import Home from '@pages/home'
+import Layout from '@pages/Layout'
+import Project from '@pages/project'
 // import NotFound from './pages/NotFound/NotFound.jsx'
 
 const App = () => {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import styles from './home.module.css';
 
-import About from "@/sections/about";
-import Skills from "@/sections/skills";
-import Experience from "@/sections/experience";
-import Education from "@/sections/education";
-import Projects from "@/sections/projects";
-import Contact from "@/sections/contact";
+import About from "@sections/about";
+import Skills from "@sections/skills";
+import Experience from "@sections/experience";
+import Education from "@sections/education";
+import Projects from "@sections/projects";
+import Contact from "@sections/contact";
 
 export default function Home() {
   return (
